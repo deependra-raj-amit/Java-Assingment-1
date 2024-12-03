8170e9b115ffcf94fa9fe3e6492240470ccde588
Deependra Raj Amit <deependra.raj.amit@gmail.com>
2024-12-03 10:29:07 +0530

    Assignment 7

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,46 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+// Class Person
+class Person1 {
+    // Attributes
+    String name;
+    int age;
+
+    // Constructor
+    public Person1(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    // Method to display details
+    public void speak() {
+        System.out.println("My name is " + name + " and I am " + age + " years old.");
+    }
+}
+
+// Class Student inheriting Person
+class Student1 extends Person1 {
+    // Additional attribute
+    int grade;
+
+    // Constructor
+    public Student1(String name, int age, int grade) {
+        super(name, age); // Initialize name and age using the superclass constructor
+        this.grade = grade;
+    }
+
+    // Method to display grade details
+    public void study() {
+        System.out.println("I am studying for grade: " + grade + ".");
+    }
+}
+
+// Main class to execute the program
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        // Create a Student object
+        Student1 student1 = new Student1("Deependra", 21, 12);
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        // Call methods
+        student1.speak();
+        student1.study();
     }
 }
